e237a889d108021ff07f9f6d44beab1d276d83a2
Marcin Filapek <filap4@gmail.com>
2022-03-01 10:45:25 +0100

    create populations and mark it

--- applications-of-artificial-intelligence-1/Program.cs
+++ applications-of-artificial-intelligence-1/Program.cs
@@ -1,17 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-using applications_of_artificial_intelligence_1;
+﻿using applications_of_artificial_intelligence_1;
 using CommandLine;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 
 await Parser.Default.ParseArguments<CommandLineOptions>(args)
     .MapResult(async (CommandLineOptions args) =>
     {
         try
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             using var reader = new StreamReader(args.Path);
 
             var array = await ReadFileAsync(reader);
+            var population = GeneratePopulation(array);
 
-            Display(array);
+            ConcurrentDictionary<int[], int> markedPopulation = MarkPopulations(population, array);
+
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time in miliseconds: {0}", stopwatch.ElapsedMilliseconds);
             return 0;
         }
         catch (Exception)
@@ -19,6 +25,48 @@ await Parser.Default.ParseArguments<CommandLineOptions>(args)
             return -3;
         }
     }, error => Task.FromResult(-1));
+
+ConcurrentDictionary<int[], int> MarkPopulations(List<int[]> populations, int[][] distances)
+{
+    ConcurrentDictionary<int[], int> result = new();
+    foreach (var population in populations)
+        result.TryAdd(population, MarkPopulation(population, distances));
+
+    return result;
+}
+
+int MarkPopulation(int[] population, int[][] distances)
+{
+    int result = 0;
+    for (int i = 0; i < population.Length; i++)
+    {
+        var j = i + 1;
+        if (j >= population.Length)
+            j = 0;
+
+        result += distances[population[i]][population[j]];    
+    }
+
+    return result;
+}
+
+List<int[]> GeneratePopulation(int[][] array)
+{
+    List<int[]> population = new();
+    var indexes = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        indexes[i] = i;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        population.Add(Randomize(indexes));
+    }
+
+    return population;
+}
+
+int[] Randomize(int[] indexes)
+    => indexes.OrderBy(_ => Random.Shared.Next()).ToArray();
 
 async ValueTask<int[][]> ReadFileAsync(StreamReader reader)
 {
@@ -51,3 +99,4 @@ void Display(int[][] array)
         Console.WriteLine();
     }
 }
+
